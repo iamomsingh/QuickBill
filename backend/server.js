67ae6c97a704063bc1930c3805ea759e3e13a07b
om singh <iamomsingh11@gmail.com>
2023-12-26 16:37:58 +0530

    backend routes completed

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log("server is rumnning....");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+// import uploadRoutes from "./routes/uploadRoutes.js";
+
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+// app.use("/api/upload", uploadRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Api is running....");
+});
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,22 @@
+// import { Container } from "react-bootstrap";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import "react-toastify/dist/ReactToastify.css";
+// import { Outlet } from "react-router-dom";
+
 function App() {
-  return <div>Welcome to task building</div>;
+  return (
+    <>
+      {/* <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer /> */}
+      <h1>HEllo</h1>
+    </>
+  );
 }
 
 export default App;

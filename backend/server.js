e5e10dd755317376386443b55fe87588a7d9b80e
om singh <iamomsingh11@gmail.com>
2023-12-26 22:19:05 +0530

    deploying

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
@@ -19,12 +20,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/api/config/paypal", (req, res) =>
-  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
-);
+const __dirname = path.resolve(); //Set __dirname to current directory
 
-app.get("/", (req, res) => {
-  res.send("Api is running....");
-});
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  //any route that is not api  will be redirected to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running.....");
+  });
+}
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
